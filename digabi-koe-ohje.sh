93d97e3a608546bb789917a1c0da8e813d83fa2b
Matti Lattu <matti.lattu@iki.fi>
2016-04-21 18:39:57 +0300

    Check that you have tools needed, added some error messages

--- digabi-koe-ohje.sh
+++ digabi-koe-ohje.sh
@@ -6,6 +6,18 @@ IS_SV=`echo "$LANG" | grep -P "^sv"`
 # Defaults to Finnish
 DOC_LANG=fi
 
+check_binary () {
+	if [ ! -x $1 ]; then
+		echo "digabi-koe-ohje: $1 is missing"
+		exit 1
+	fi
+}
+
+# Check that needed tools are available
+check_binary /usr/bin/yelp
+check_binary /usr/bin/pgrep
+check_binary /usr/bin/xdotool
+
 if [ "${IS_FI}" != "" ]; then
 	echo "Finnish language selected"
 	DOC_LANG=fi
@@ -18,7 +30,7 @@ elif [ "${IS_SV}" != "" ]; then
 		DOC_LANG=fi
 	fi
 else
-	echo "No known language selected, defaults to ${DOC_LANG}"
+	echo "$0: No known language selected, defaults to ${DOC_LANG}"
 fi
 
 # Kill existing yelps
@@ -32,8 +44,8 @@ YELP_WID=
 try_count=0
 
 while [[ -z $YELP_WID && $try_count -lt 100 ]]; do
-	YELP_PID=`pgrep -f /usr/bin/yelp`
-	YELP_WID=`xdotool search --all --onlyvisible --pid ${YELP_PID} 2>/dev/null | grep -P "^\d+$" | head -n 1`
+	YELP_PID=`/usr/bin/pgrep -f /usr/bin/yelp`
+	YELP_WID=`/usr/bin/xdotool search --all --onlyvisible --pid ${YELP_PID} 2>/dev/null | grep -P "^\d+$" | head -n 1`
 	let "try_count=${try_count}+1"
 done
 
@@ -41,4 +53,6 @@ done
 if [ "${YELP_WID}" != "" ]; then
 	xdotool windowsize --sync ${YELP_WID} 800 500
 	xdotool windowmove --sync ${YELP_WID} 25 25
+else
+	echo "$0: Could not get WindowID for /usr/bin/yelp, could not adjust window"
 fi
