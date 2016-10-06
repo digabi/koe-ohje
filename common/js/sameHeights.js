5a8004c20005b1550f3e96a5c5daeedef16a56ef
Matti Lattu <matti.lattu@iki.fi>
2016-10-06 10:58:28 +0300

    Graphic Design by Jutta

---
+++ common/js/sameHeights.js
@@ -0,0 +1,40 @@
+function sameHeights(selector) {
+	console.log("sameHeights started");
+	// Loop over given tables
+	$(selector).each(function(index) {
+		$("tr").each(function(index) {
+			// Look for max row height
+			var max_height = 0;
+			$("td").each(function(index) {
+				if ($(this).height() > max_height) {
+					max_height = $(this).height();
+				}
+			});
+			// Set row height for all elements in this row
+			$("td").each(function(index) {
+				$(this).height(max_height);
+			});
+		})
+	});
+	console.log("sameHeights ended");
+}
+
+function setSameHeights () {
+	// Formula tables
+	sameHeights(".formulae");
+}
+
+if ('addEventListener' in window) {
+	window.addEventListener('resize', function(){
+		setSameHeights();
+	});
+	window.addEventListener('load', function(){
+		setSameHeights();
+	});
+}
+
+// Call setSameHeights() when MathJax typesetting is finished
+MathJax.Hub.Register.StartupHook("End",function () {
+	console.log("MathJax is ready");
+	setSameHeights();
+});
