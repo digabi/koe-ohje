25c48ccf7794ccde70a261971771a94944ead384
Matti Lattu <matti.lattu@iki.fi>
2017-03-22 15:28:40 +0200

    Wrap tab JS to an object, apply to top-level tabs

--- common/js/activetab.js
+++ common/js/activetab.js
@@ -1,34 +1,37 @@
-function tab_initialise (tab_name) {
-    if (tab_name == "tables") {
-        setSameHeights();
-    }
-}
+var ActiveTab = {
+	tabNames : [],
+	classPrefix : null,
+	activeTabIndex : -1,
+	activeTabInitialise : {},
+	activateTabs : function () {
+		var self = this;
+		
+		$("."+this.classPrefix+"-menu > li").click(function(e){
+			var curname_arr = e.target.id.split("-");
 
-$(document).ready(function(){
-    var activeTabIndex = -1;
-    var tabNames = ["shortcut","char_pc","char_mac","videos","tables"];
+			var new_tab = curname_arr[0];
+			var new_lang = curname_arr[1];
 
-    $(".tab-menu > li").click(function(e){
-        var curname_arr = e.target.id.split("-");
+			for(var i=0; i < self.tabNames.length; i++) {
+				if(new_tab == self.tabNames[i]) {
+					self.activeTabIndex = i;
+				} else {
+					$("#"+self.tabNames[i]+'-'+new_lang).removeClass("active");
+					$("#"+self.classPrefix+"-"+self.tabNames[i]).css("display", "none");
+				}
+			}
+			$("#"+self.classPrefix+"-"+self.tabNames[self.activeTabIndex]).fadeIn();
+			$("#"+new_tab+'-'+new_lang).addClass("active");
 
-        var new_tab = curname_arr[0];
-        var new_lang = curname_arr[1];
+			// Execute possible tab-specific initialisation function
+			if (typeof(self.activeTabInitialise[new_tab]) == "function") {
+				console.log("Executing tab-specific initialisation for: "+new_tab);
+				self.activeTabInitialise[new_tab]();
+			}
 
-        for(var i=0;i<tabNames.length;i++) {
-            if(new_tab == tabNames[i]) {
-                activeTabIndex = i;
-            } else {
-                $("#"+tabNames[i]+'-'+new_lang).removeClass("active");
-                $("#tab-"+tabNames[i]).css("display", "none");
-            }
-        }
-        $("#tab-"+tabNames[activeTabIndex]).fadeIn();
-        $("#"+new_tab+'-'+new_lang).addClass("active");
-
-        tab_initialise(new_tab);
-
-        return false;
-    });
-    
-    $(".tab-menu > li").css('cursor', 'pointer');
-});
+			return false;
+		});
+		
+		$("."+self.classPrefix+"-menu > li").css('cursor', 'pointer');
+	}
+};
