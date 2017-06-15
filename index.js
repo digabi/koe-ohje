8b4217fae2c4df1114e2e628bdf1ff9516ac2b60
Kristian Wahlroos <kristian.wahlroos@reaktor.fi>
2017-06-15 13:31:27 +0300

    add current configurations

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const fs = require('fs-extra');
 const input = fs.readFileSync('./content/taulukot/tab-math.html');
 
 const buildFolder = 'build/'
+const pageConfig = {
+    format: ["TeX"],
+    singleDollars: true
+}
+const nodeConfig = {
+    svg: true,
+    linebreaks: true,
+    MathJax: {
+        imageFont: null,
+	    CommonHTML: {
+		    scale: 90
+	    }
+    }
+}
 
 fs.ensureDir(buildFolder)
     .then(() => {
-        mjpage(input, { format: ["TeX"] }, { svg: true }, function (output) {
+        mjpage(input, pageConfig, nodeConfig, function (output) {
 
             fs.writeFile(buildFolder + "hontsa.html", output, function (err) {
                 if (err) {
