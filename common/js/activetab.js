7a8896538877bbaf48cfd922ce1955fc914bcc17
Kristian Wahlroos <kristian.wahlroos@reaktor.fi>
2017-06-19 13:48:15 +0300

    reformat, lazy load and copy

--- common/js/activetab.js
+++ common/js/activetab.js
@@ -1,43 +1,43 @@
 var ActiveTab = {
-	tabNames : [],
-	classPrefix : null,
-	activeTabIndex : -1,
-	activeTabInitialise : {},
-	activateTabs : function () {
+	tabNames: [],
+	classPrefix: null,
+	activeTabIndex: -1,
+	activeTabInitialise: {},
+	activateTabs: function () {
 		var self = this;
-		
-		$("."+this.classPrefix+"-menu-option").click(function(e){
+
+		$("." + this.classPrefix + "-menu-option").click(function (e) {
 			var curname_arr = e.target.id.split("-");
 
 			var new_tab = curname_arr[0];
 			var new_lang = curname_arr[1];
-			
+
 			// Show new menu entry
-			$("#"+self.classPrefix+"-menu #"+new_tab+'-'+new_lang).addClass(self.classPrefix+"-menu-option-active");
+			$("#" + self.classPrefix + "-menu #" + new_tab + '-' + new_lang).addClass(self.classPrefix + "-menu-option-active");
 
 			// Hide menu entry and tab content
-			for(var i=0; i < self.tabNames.length; i++) {
-				if(new_tab == self.tabNames[i]) {
+			for (var i = 0; i < self.tabNames.length; i++) {
+				if (new_tab == self.tabNames[i]) {
 					self.activeTabIndex = i;
 				} else {
-					$("#"+self.classPrefix+"-menu #"+self.tabNames[i]+'-'+new_lang).removeClass(self.classPrefix+"-menu-option-active");
-					$("#"+self.classPrefix+"-"+self.tabNames[i]).css("display", "none");
+					$("#" + self.classPrefix + "-menu #" + self.tabNames[i] + '-' + new_lang).removeClass(self.classPrefix + "-menu-option-active");
+					$("#" + self.classPrefix + "-" + self.tabNames[i]).css("display", "none");
 				}
 			}
-			
+
 			// Show new tab content
-			$("#"+self.classPrefix+"-"+self.tabNames[self.activeTabIndex]).css("display", "block");
+			$("#" + self.classPrefix + "-" + self.tabNames[self.activeTabIndex]).css("display", "block");
 
 			// Execute possible tab-specific initialisation function
-			if (typeof(self.activeTabInitialise[new_tab]) == "function") {
-				console.log("Executing tab-specific initialisation for: "+new_tab);
+			if (typeof (self.activeTabInitialise[new_tab]) == "function") {
+				console.log("Executing tab-specific initialisation for: " + new_tab);
 				self.activeTabInitialise[new_tab]();
 			}
 
 			return false;
 		});
-	
-	// Show default tab content
-	$("."+this.classPrefix+"-content.active").css("display", "block");
+
+		// Show default tab content
+		$("." + this.classPrefix + "-content.active").css("display", "block");
 	}
 };
