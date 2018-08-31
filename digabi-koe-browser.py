d7c5674f6678264a2c249851c3eb6ba4c46d73d9
Timo Metsälä <timo.metsala@reaktor.com>
2018-08-31 10:37:35 +0300

    Porting to Qt5, part i

--- digabi-koe-browser.py
+++ digabi-koe-browser.py
@@ -3,9 +3,11 @@
 
 import argparse, sys, subprocess
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from PyQt4.QtWebKit import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebKit import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebKitWidgets import QWebView
 
 from os.path import expanduser
 
