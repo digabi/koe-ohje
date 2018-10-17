b8d893a0cea7ce059ff5385d7da0655e36d9f2ed
Matti Lattu <matti.lattu@iki.fi>
2018-10-17 18:23:25 +0300

    Replaced standard scrolling to anchor with luxus solution to show headings behind the upper navi

--- common/js/tableOfContents.js
+++ common/js/tableOfContents.js
@@ -24,11 +24,8 @@ function initializeTocBot(language) {
     // Toggle toc menu (#menu button is show on narrow displays)
     $("#menu").off("click");
     $("#menu").click(function() {
-      $(".js-toc").toggle("fast", function () {
-        $(".toc-link").click(function() {
-          $(".js-toc").hide("fast");
-        });
-      });
+      $(".js-toc").toggle("fast");
+      setTocLinkClick();
     });
 
     // Close toc menu when tab is changed on narrow displays
@@ -44,16 +41,63 @@ function initializeTocBot(language) {
     });
 
     showTocBasedOnWidth();
+
+    // Handle toc links here instead of default behaviour
+    setTocLinkClick();
 }
 
 function showTocBasedOnWidth () {
   if ($(window).width()>1024) {
     $(".js-toc").show();
     $(".js-toc").css('top', $("#tab-menu").height());
-    $(".toc-link").off("click");
+    setTocLinkClick();
   }
   else {
     $(".js-toc").hide();
     $(".js-toc").css('top', $("#tab-menu").height());
   }
+}
+
+function setTocLinkClick () {
+  $("a.toc-link").off("click");
+
+  $("a.toc-link").click(function (event) {
+    if ($("#menu").css('display') == 'block') {
+      $(".js-toc").hide("fast");
+    }
+
+    // Scroll to given ID (take account the upper navi) and prevent/revert to default action as needed
+    return scrollToElement(event.target.hash);
+  });
+}
+
+function getTocLinkId (linkURI) {
+  // Return ID (anchor) part of the URL
+  if (linkURI.indexOf("#") < 0) {
+    return null;
+  }
+
+  return linkURI.substring(linkURI.indexOf("#"));
+}
+
+function scrollToElement (targetID) {
+  if (targetID == null || targetID == "") {
+    console.log("Could not get targetID for TOC link, reverting to default action");
+    return true;
+  }
+
+  if ($(targetID).length != 1) {
+    console.log("TargetID points to " + $(targetID).length + " elements, reverting to default action");
+    return true;
+  }
+
+  var bodyRect = document.body.getBoundingClientRect(),
+    elemRect = $(targetID).get(0).getBoundingClientRect(),
+    offset   = elemRect.top - bodyRect.top;
+
+  // Scroll to 50 px above the given ID
+  window.scrollTo(0, offset-50);
+
+  // Prevent default action
+  return false;
 }
