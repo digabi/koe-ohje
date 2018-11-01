a468a97ee5f144963aa6b8c316b87228f87c5342
Eero Pitkänen <eero.pitkanen@reaktor.com>
2018-11-01 17:42:05 +0200

    Added P and LI tags to search, modified search result rendering
    Added P and LI tags to the search, modified the MathJax search results to use the already rendered SVGs and content, added background color to even results to distinguish different results better from each other

--- common/js/search.js
+++ common/js/search.js
@@ -1,7 +1,7 @@
 var searchIdCounter = 0;
 
 function searchResults(searchTerm, hitsMax) {
-  var SEARCH_SELECTOR = "h1, h2, h3, th, td";
+  var SEARCH_SELECTOR = "h1, h2, h3, th, td, p, li:not(.toc-list-item, .search-result-item)";
   var SEARCH_RESULT_MAXLENGTH = 60;
 
   var searchResults = [];
@@ -18,19 +18,26 @@ function searchResults(searchTerm, hitsMax) {
         var text = $(this).text();
         if (text.indexOf("\\") > -1) {
           // This is a formula
-          text = "\\("+text+"\\)";
+          //text = "\\("+text+"\\)";
+          // Get the block containing the formula in rendered form
+          text = $(this).html();
         }
         else if (text.length > SEARCH_RESULT_MAXLENGTH) {
           // Cut long strings
           text = text.substring(0, SEARCH_RESULT_MAXLENGTH-3) + "...";
         }
 
+        var resultPrefix = ""		
+		if( $(this).context.tagName=="H2" || $(this).context.tagName=="H3" ){
+		  resultPrefix = " \u2261 ";
+		}
+
         if ($(this).attr('id') != undefined) {
-          searchResults.push("<li class='search-result-item'><a href='#"+$(this).attr('id')+"' class='search-result-link'>"+text+"</a></li>");
+          searchResults.push("<li class='search-result-item'>"+resultPrefix+"<a href='#"+$(this).attr('id')+"' class='search-result-link'>"+text+"</a></li>");
         }
         else {
           var parentId = searchSetRandomID($(this));
-          searchResults.push("<li class='search-result-item'><a href='#"+parentId+"' class='search-result-link'>"+text+"</a></li>");
+          searchResults.push("<li class='search-result-item'>"+resultPrefix+"<a href='#"+parentId+"' class='search-result-link'>"+text+"</a></li>");
         }
 
         if (searchResults.length > hitsMax) {
