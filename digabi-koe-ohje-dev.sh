4e1b95e9f88dd373a21123d0167d849ef97bb64d
Sami Saves <sami.saves@reaktor.com>
2020-03-19 16:21:27 +0200

    Add script to launch koe-ohje in abikit browser

---
+++ digabi-koe-ohje-dev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+IS_FI=`echo "$LANG" | grep -P "^fi"`
+IS_SV=`echo "$LANG" | grep -P "^sv"`
+
+# Defaults to Finnish
+DOC_LANG=fi
+TITLE="Koejärjestelmän ohjeet"
+
+if [ "${IS_FI}" != "" ]; then
+	echo "Finnish language selected"
+	DOC_LANG=fi
+elif [ "${IS_SV}" != "" ]; then
+	echo "Swedish language selected"
+	DOC_LANG=sv
+	TITLE="Provmiljöns instruktioner"
+
+	if [ ! -f /usr/local/share/digabi-koe-ohje/build/index-${DOC_LANG}.html ]; then
+		echo "Oops, Swedish documentation is missing"
+		DOC_LANG=fi
+		TITLE="Koejärjestelmän ohjeet"
+	fi
+else
+	echo "$0: No known language selected, defaults to ${DOC_LANG}"
+fi
+
+# Kill existing browsers
+pkill koeohje
+
+abikit-browser -t "${TITLE}" -W 1045 -H 600 -x 30 -y 30 \
+	-n koeohje -i ./help-browser.svg \
+	"file://${PWD}/build/index.html" &
