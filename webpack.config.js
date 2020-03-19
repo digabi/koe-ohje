9c890c903c79b9da49dca78fa0145311d23a4c25
Sami Saves <sami.saves@reaktor.com>
2020-03-19 15:10:42 +0200

    Add webpack dev server for running the project locally

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new CopyPlugin([
+      { from: 'common', to: 'common' },
+      { from: 'build', to: '' }
+    ])
+  ],
+  devServer: {
+    compress: true,
+    port: 8080
+  }
+}
