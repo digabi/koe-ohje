f97dd0ca31c85f07a1a2295e62cb50859a98cde3
Sami Saves <sami.saves@reaktor.com>
2020-03-23 13:20:53 +0200

    Remove inline source maps from production

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
 const path = require('path')
 const express = require('express')
+module.exports = (env, argv) => {
+  const isProduction = argv && argv['mode'] === 'production'
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true
-          }
+  return {
+    entry: './src/index.ts',
+    devtool: isProduction ? 'cheap-module-source-map' : 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          },
+          include: path.resolve(__dirname, 'src')
         },
-        include: path.resolve(__dirname, 'src')
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        include: path.resolve(__dirname, 'src')
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [{ loader: 'file-loader' }],
-        include: path.resolve(__dirname, 'src')
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+          include: path.resolve(__dirname, 'src')
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [{ loader: 'file-loader' }],
+          include: path.resolve(__dirname, 'src')
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    devServer: {
+      compress: true,
+      port: 8080,
+      contentBase: path.resolve('build'),
+      before(app) {
+        app.use('/common', express.static('common'))
       }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  devServer: {
-    compress: true,
-    port: 8080,
-    contentBase: path.resolve('build'),
-    before(app) {
-      app.use('/common', express.static('common'))
     }
   }
 }
