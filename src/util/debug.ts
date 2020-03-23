46dc60c749e63c0ee89a65add8be09bc5ece16f8
Sami Saves <sami.saves@reaktor.com>
2020-03-23 10:04:38 +0200

    Make printing into abikit cleaner

--- src/util/debug.ts
+++ src/util/debug.ts
@@ -3,7 +3,8 @@ import { isAbikitBrowser } from './abikit'
 export const log = (message: any, ...optionalParameters: any[]) => {
   if (isAbikitBrowser()) {
     const messageString = typeof message !== 'string' ? JSON.stringify(message) : message
-    window.sharedclass.write_to_stdout(messageString + '\n' + JSON.stringify(optionalParameters))
+    const optionalString = optionalParameters.length > 0 ? JSON.stringify(optionalParameters) : ''
+    window.sharedclass.write_to_stdout(messageString + optionalString)
   } else {
     console.log(message, ...optionalParameters)
   }
