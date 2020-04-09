31a9ca6b4af9c3c4def421d39ca7ac243ace31ad
Sami Saves <sami.saves@reaktor.com>
2020-04-09 14:13:21 +0300

    Add tests for opening all tabs

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -8,13 +8,76 @@ describe('Digabi Exam Help', () => {
       await expect(page).toMatch('Yleisohjeet')
     })
 
-    it('should be able to change tabs', async () => {
+    it('should be able to open spanish tabs', async () => {
       await page.evaluate(() => {
         const el: HTMLElement = document.querySelector('#spanish-fi .tab-menu-option-long')
         el.click()
       })
 
       await expect(page).toMatchElement('#tab-spanish h1')
+    })
+
+    it('should be able to open physics tab', async () => {
+      await page.evaluate(() => {
+        const el: HTMLElement = document.querySelector('#physics-fi .tab-menu-option-long')
+        el.click()
+      })
+
+      await expect(page).toMatchElement('#tab-physics h1', { timeout: 5000 })
+    })
+
+    it('should be able to open chemistry tab', async () => {
+      await page.evaluate(() => {
+        const el: HTMLElement = document.querySelector('#chemistry-fi .tab-menu-option-long')
+        el.click()
+      })
+
+      await expect(page).toMatchElement('#tab-chemistry h1', { timeout: 5000 })
+    })
+
+    it('should be able to open math tab', async () => {
+      await page.evaluate(() => {
+        const el: HTMLElement = document.querySelector('#math-fi .tab-menu-option-long')
+        el.click()
+      })
+
+      await expect(page).toMatchElement('#tab-math h1', { timeout: 5000 })
+    })
+
+    it('should be able to open french tab', async () => {
+      await page.evaluate(() => {
+        const el: HTMLElement = document.querySelector('#french-fi .tab-menu-option-long')
+        el.click()
+      })
+
+      await expect(page).toMatchElement('#tab-french h1')
+    })
+
+    it('should be able to open german tab', async () => {
+      await page.evaluate(() => {
+        const el: HTMLElement = document.querySelector('#german-fi .tab-menu-option-long')
+        el.click()
+      })
+
+      await expect(page).toMatchElement('#tab-german h1')
+    })
+
+    it('should be able to open sami tab', async () => {
+      await page.evaluate(() => {
+        const el: HTMLElement = document.querySelector('#sami-fi .tab-menu-option-long')
+        el.click()
+      })
+
+      await expect(page).toMatchElement('#tab-sami h1')
+    })
+
+    it('should be able to open geography tab', async () => {
+      await page.evaluate(() => {
+        const el: HTMLElement = document.querySelector('#geography-fi .tab-menu-option-long')
+        el.click()
+      })
+
+      await expect(page).toMatchElement('#tab-geography h1')
     })
   })
 
