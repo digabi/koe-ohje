52a029c1842a2c8b7b3e9fc9c25d0995775acad4
Sami Saves <sami.saves@reaktor.com>
2020-05-28 13:29:37 +0300

    Enable dev tools in start:abikit command

--- digabi-koe-ohje-dev.sh
+++ digabi-koe-ohje-dev.sh
@@ -7,6 +7,6 @@ TITLE="Koejärjestelmän ohjeet"
 # Kill existing browsers
 pkill koeohje
 
-abikit-browser -t "${TITLE}" -W 1045 -H 600 -x 30 -y 30 \
+abikit-browser -dev True -t "${TITLE}" -W 1045 -H 600 -x 30 -y 30 \
 	-n koeohje -i ./help-browser.svg \
 	"file://${PWD}/build/index.html" &
