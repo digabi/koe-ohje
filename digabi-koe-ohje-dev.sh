ae4ad156b8002d3666c596876b9769f99dfbbe93
Sami Saves <sami.saves@reaktor.com>
2020-05-29 15:21:36 +0300

    Connect abikit to localhost

--- digabi-koe-ohje-dev.sh
+++ digabi-koe-ohje-dev.sh
@@ -9,4 +9,4 @@ pkill koeohje
 
 abikit-browser -dev True -t "${TITLE}" -W 1045 -H 600 -x 30 -y 30 \
 	-n koeohje -i ./help-browser.svg \
-	"file://${PWD}/build/index.html" &
+	"http://localhost:8080/build/index.html?fi" &
