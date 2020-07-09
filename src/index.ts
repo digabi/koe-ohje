8c985efc8a949d44f9aee0976b332a6162c77612
Sami Saves <sami.saves@reaktor.com>
2020-07-09 14:47:44 +0300

    Remove legacy integration

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
 import './polyfills'
-import { initializeCopyToClipboard } from './tabs/common/clipboard'
-import { initializeGeographyTab } from './tabs/geography'
 import { initializeLanguage } from './tabs/common/language'
 import { initializeTabs } from './tabs/tabs'
 import { initializeTocEventListeners } from './tabs/common/toc'
 import { initializeSearchEventListeners } from './tabs/common/search'
-
-declare global {
-  interface Window {
-    legacyIntegration: any
-  }
-}
-
-window.legacyIntegration = {}
-window.legacyIntegration.initializeCopyToClipboard = initializeCopyToClipboard
-window.legacyIntegration.initializeLanguage = initializeLanguage
-window.legacyIntegration.initializeGeographyTab = initializeGeographyTab
 
 window.addEventListener('load', () => {
   initializeLanguage()
