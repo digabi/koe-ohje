1f06e2b8bc00c69360535fcf8c5c077222de7db4
Matti Lattu <matti.lattu@iki.fi>
2021-12-22 15:36:08 +0200

    Store selected language to localStorage

--- src/tabs/common/language.ts
+++ src/tabs/common/language.ts
@@ -3,11 +3,37 @@ export enum Language {
   swedish = 'sv'
 }
 
+const localStorageLanguageKey = 'digabi-koe-ohje-last-language'
+
+const setPreviousLanguage = (language: Language) => {
+  window.localStorage.setItem(localStorageLanguageKey, language)
+}
+
+const getPreviousLanguage = (): Language => {
+  return window.localStorage.getItem(localStorageLanguageKey)
+}
+
 export const getCurrentLanguage = () => {
-    const url = new URL(window.location.href)
-    if (url.searchParams.get(Language.swedish) !== null) return Language.swedish
+  // Check language from the URL
+  const url = new URL(window.location.href)
+  if (url.searchParams.get(Language.finnish) !== null) {
+    setPreviousLanguage(Language.finnish)
     return Language.finnish
-  
+  }
+  if (url.searchParams.get(Language.swedish) !== null) {
+    setPreviousLanguage(Language.swedish)
+    return Language.swedish
+  }
+
+  // Try to get previous language code
+  const previousLanguage = getPreviousLanguage()
+  let languages = Object.values(Language)
+  if (languages.indexOf(previousLanguage) >= 0) {
+    return previousLanguage
+  }
+
+  // Defaults to Finnish
+  return Language.finnish
 }
 
 export const initializeLanguage = () => {
