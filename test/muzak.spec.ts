c6d746504cab30293837454a7a1cfcf45e6dde47
Matti Lattu <matti.lattu@iki.fi>
2021-12-22 15:02:38 +0200

    Initial tests for muzak page

---
+++ test/muzak.spec.ts
@@ -0,0 +1,36 @@
+describe('Muzak', () => {
+  describe('Muzak Player', () => {
+    beforeEach(async () => {
+      await page.goto('http://localhost:8080/build/index.html?fi')
+
+      await page.evaluate(() => {
+        const el = document.querySelector<HTMLElement>('#tab-menu div[data-tab-id=muzak]')
+        el.click()
+      })
+
+      // Wait for tab change
+      await expect(page).toMatchElement('#tab-muzak h1', { timeout: 5000 })
+    })
+
+    it('should contain fontawesome play icon', async () => {
+      const buttonHTML = await page.evaluate(() => {
+        const el = document.querySelector<HTMLElement>('[data-muzakid="tab-muzak-music1"]')
+        return el.innerHTML
+      })
+      expect(buttonHTML).toContain('<!-- <i class="fas fa-play"></i> Font Awesome fontawesome.com -->')
+    })
+
+    it('should change play button to pause', async () => {
+      await page.evaluate(() => {
+        const el = document.querySelector<HTMLElement>('[data-muzakid="tab-muzak-music1"]')
+        el.click()
+      })
+
+      const buttonHTML = await page.evaluate(() => {
+        const el = document.querySelector<HTMLElement>('[data-muzakid="tab-muzak-music1"]')
+        return el.innerHTML
+      })
+      expect(buttonHTML).toContain('<!-- <i class="fas fa-pause"></i> Font Awesome fontawesome.com -->')
+    })
+  })
+})
