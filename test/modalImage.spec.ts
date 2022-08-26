58430c19b0dff3910d129faeb3c76a3e3098bd38
Matti Lattu <matti.lattu@iki.fi>
2022-08-26 13:50:20 +0300

    Use backtick strings instead of concat

--- test/modalImage.spec.ts
+++ test/modalImage.spec.ts
@@ -18,31 +18,31 @@ describe('ModalImages', () => {
     beforeEach(async () => await page.goto('http://localhost:8080/build/index.html?fi'))
 
     it('should have the modal layer hidden before the thumbnail image is clicked', async () => {
-      await expect(page.locator('#modal-image-id-' + TEST_IMAGE_ID)).toBeHidden()
+      await expect(page.locator(`#modal-image-id-${TEST_IMAGE_ID}`)).toBeHidden()
     })
 
     it('should open and close (by clicking the close button) the modal layer', async () => {
-      await page.click('#' + TEST_IMAGE_ID) // Must equal TEST_IMAGE_ID
-      await expect(page.locator('#modal-image-id-' + TEST_IMAGE_ID)).toBeVisible()
+      await page.click(`#${TEST_IMAGE_ID}`)
+      await expect(page.locator(`#modal-image-id-${TEST_IMAGE_ID}`)).toBeVisible()
 
       await page.click('.modal-image-close')
-      await expect(page.locator('#modal-image-id-' + TEST_IMAGE_ID)).toBeHidden()
+      await expect(page.locator(`#modal-image-id-${TEST_IMAGE_ID}`)).toBeHidden()
     })
 
     it('should open and close (by clicking the modal image) the modal layer', async () => {
-      await page.click('#tab-gen-image-03') // Must equal TEST_IMAGE_ID
-      await expect(page.locator('#modal-image-id-' + TEST_IMAGE_ID)).toBeVisible()
+      await page.click(`#${TEST_IMAGE_ID}`)
+      await expect(page.locator(`#modal-image-id-${TEST_IMAGE_ID}`)).toBeVisible()
 
       await page.click('.modal-image-fullscreen-image')
-      await expect(page.locator('#modal-image-id-' + TEST_IMAGE_ID)).toBeHidden()
+      await expect(page.locator(`#modal-image-id-${TEST_IMAGE_ID}`)).toBeHidden()
     })
 
     it('should open and close (by clicking the modal background) the modal layer', async () => {
-      await page.click('#tab-gen-image-03') // Must equal TEST_IMAGE_ID
-      await expect(page.locator('#modal-image-id-' + TEST_IMAGE_ID)).toBeVisible()
+      await page.click(`#${TEST_IMAGE_ID}`)
+      await expect(page.locator(`#modal-image-id-${TEST_IMAGE_ID}`)).toBeVisible()
 
       await page.click('.modal-image-fullscreen')
-      await expect(page.locator('#modal-image-id-' + TEST_IMAGE_ID)).toBeHidden()
+      await expect(page.locator(`#modal-image-id-${TEST_IMAGE_ID}`)).toBeHidden()
     })
   })
 })
