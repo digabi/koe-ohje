8c6a833530623c9b6cf86709d4ca5992ffc7de37
Heikki Honkanen <heikki.honkanen@reaktor.com>
2024-12-11 09:58:22 +0200

    Run tests with esbuild watch
    The added slash in the navigation commands seems to just be
    a difference in how webpack and esbuild serve stuff. Similat
    attempts at navigation as the old test implementation don't
    work in real environments.

--- test/language.spec.ts
+++ test/language.spec.ts
@@ -3,103 +3,103 @@ import { test, expect } from '@playwright/test'
 test.describe('Language', () => {
   test.describe('Initial language selection', () => {
     test('should highlight finnish in navbar', async ({ page }) => {
-      await page.goto('/build?fi')
+      await page.goto('/build/?fi')
 
       await expect(page.locator('.tab-menu-language-fi.active')).toBeVisible()
       await expect(page.locator('.tab-menu-language-sv.active')).not.toBeVisible()
     })
 
     test('should highlight swedish in navbar', async ({ page }) => {
-      await page.goto('/build?sv')
+      await page.goto('/build/?sv')
 
       await expect(page.locator('.tab-menu-language-fi.active')).not.toBeVisible()
       await expect(page.locator('.tab-menu-language-sv.active')).toBeVisible()
     })
 
     test('should have correct page title', async ({ page }) => {
-      await page.goto('/build?fi')
+      await page.goto('/build/?fi')
       await expect(page).toHaveTitle('Koeympäristön ohjeet')
 
-      await page.goto('/build?sv')
+      await page.goto('/build/?sv')
       await expect(page).toHaveTitle('Provmiljöns instruktioner')
     })
   })
 
   test.describe('Default tab', () => {
     test('should show general tab in finnish', async ({ page }) => {
-      await page.goto('/build?fi&general')
+      await page.goto('/build/?fi&general')
       await expect(page.locator('h1')).toHaveText('Yleisohjeet')
     })
 
     test('should show general tab in swedish', async ({ page }) => {
-      await page.goto('/build?sv&general')
+      await page.goto('/build/?sv&general')
       await expect(page.locator('h1')).toHaveText('Allmänna instruktioner')
     })
 
     test('should show chemistry tab in finnish', async ({ page }) => {
-      await page.goto('/build?fi&chemistry')
+      await page.goto('/build/?fi&chemistry')
       await expect(page.locator('h1')).toHaveText('Kemia')
     })
 
     test('should show chemistry tab in swedish', async ({ page }) => {
-      await page.goto('/build?sv&chemistry')
+      await page.goto('/build/?sv&chemistry')
       await expect(page.locator('h1')).toHaveText('Kemi')
     })
 
     test('should show maps tab in finnish', async ({ page }) => {
-      await page.goto('/build?fi&maps')
+      await page.goto('/build/?fi&maps')
       await expect(page.locator('h1')).toHaveText('Kartat')
     })
 
     test('should show maps tab in swedish', async ({ page }) => {
-      await page.goto('/build?sv&maps')
+      await page.goto('/build/?sv&maps')
       await expect(page.locator('h1')).toHaveText('Kartor')
     })
 
     test('should show keyboard tab in finnish', async ({ page }) => {
-      await page.goto('/build?fi&keyboard')
+      await page.goto('/build/?fi&keyboard')
       await expect(page.locator('h1')).toHaveText('Näppäimistö')
     })
 
     test('should show keyboard tab in swedish', async ({ page }) => {
-      await page.goto('/build?sv&keyboard')
+      await page.goto('/build/?sv&keyboard')
       await expect(page.locator('h1')).toHaveText('Tangentbord')
     })
 
     test('should show math tab in finnish', async ({ page }) => {
-      await page.goto('/build?fi&math')
+      await page.goto('/build/?fi&math')
       await expect(page.locator('h1')).toHaveText('Matematiikka')
     })
 
     test('should show math tab in swedish', async ({ page }) => {
-      await page.goto('/build?sv&math')
+      await page.goto('/build/?sv&math')
       await expect(page.locator('h1')).toHaveText('Matematik')
     })
 
     test('should show physics tab in finnish', async ({ page }) => {
-      await page.goto('/build?fi&physics')
+      await page.goto('/build/?fi&physics')
       await expect(page.locator('h1')).toHaveText('Fysiikka')
     })
 
     test('should show physics tab in swedish', async ({ page }) => {
-      await page.goto('/build?sv&physics')
+      await page.goto('/build/?sv&physics')
       await expect(page.locator('h1')).toHaveText('Fysik')
     })
 
     test('should show programming tab in finnish', async ({ page }) => {
-      await page.goto('/build?fi&programming')
+      await page.goto('/build/?fi&programming')
       await expect(page.locator('h1')).toHaveText('Ohjelmointi')
     })
 
     test('should show programming tab in swedish', async ({ page }) => {
-      await page.goto('/build?sv&programming')
+      await page.goto('/build/?sv&programming')
       await expect(page.locator('h1')).toHaveText('Programmering')
     })
   })
 
   test.describe('Changing language', () => {
     test('should change default page language from finnish to swedish when clicked', async ({ page }) => {
-      await page.goto('/build?fi&general')
+      await page.goto('/build/?fi&general')
 
       await expect(page.locator('h1')).toHaveText('Yleisohjeet')
 
@@ -109,7 +109,7 @@ test.describe('Language', () => {
     })
 
     test('should change default page language from swedish to finnish when clicked', async ({ page }) => {
-      await page.goto('/build?sv&general')
+      await page.goto('/build/?sv&general')
 
       await expect(page.locator('h1')).toHaveText('Allmänna instruktioner')
 
