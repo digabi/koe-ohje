4a4ed39135bac75e3b2efb02a79b494b31f56b30
Heikki Mantere <heikki.mantere@gmail.com>
2025-10-03 12:28:22 +0300

    Refactor

--- src/tabs/common/versionSelector.ts
+++ src/tabs/common/versionSelector.ts
@@ -4,32 +4,8 @@ import { initializeLanguage } from './language'
 import { initializeToc } from './toc'
 
 type Version = '1' | '2'
+
 export const initializeVersionSelector = (tabName: string) => {
-  async function loadVersion(version: Version): Promise<void> {
-    const contentElement = document.getElementById(`content-instructions-${tabName}`)
-    if (contentElement) {
-      contentElement.innerHTML = ''
-      contentElement.className = ''
-      contentElement.classList.add(`abitti-${version}`)
-
-      if (tabName === 'general') {
-        const html = await loadHtml(`tab-${tabName}-a${version}.html`)
-        contentElement.innerHTML = html
-        const modalImages = contentElement.querySelectorAll<Element>('.tab-gen-modal-image')
-        modalImages.forEach((element: Element) => activateModalImage(element))
-      }
-      if (tabName === 'keyboard') {
-        const html = await loadHtml(`tab-${tabName}-a1-a2.html`)
-        contentElement.innerHTML = html
-      }
-
-      const hiddenElements = contentElement.querySelectorAll<HTMLElement>(`.hide-abitti-${version}`)
-      hiddenElements.forEach((element) => element.parentNode?.removeChild(element))
-
-      initializeLanguage()
-    }
-  }
-
   const queryParams = new URLSearchParams(window.location.search)
   const abittiVersion = queryParams.get('abittiVersion')
   const buttons = document.querySelectorAll('.abitti-version-button')
@@ -53,5 +29,30 @@ export const initializeVersionSelector = (tabName: string) => {
 
     document.querySelectorAll('.abitti-version-button').forEach((b) => b.classList.remove('active'))
     document.querySelector(`[data-abitti-version="${version}"]`)?.classList.add('active')
+  }
+
+  async function loadVersion(version: Version): Promise<void> {
+    const contentElement = document.getElementById(`content-instructions-${tabName}`)
+    if (contentElement) {
+      contentElement.innerHTML = ''
+      contentElement.className = ''
+      contentElement.classList.add(`abitti-${version}`)
+
+      if (tabName === 'general') {
+        const html = await loadHtml(`tab-${tabName}-a${version}.html`)
+        contentElement.innerHTML = html
+        const modalImages = contentElement.querySelectorAll<Element>('.tab-gen-modal-image')
+        modalImages.forEach((element: Element) => activateModalImage(element))
+      }
+      if (tabName === 'keyboard') {
+        const html = await loadHtml(`tab-${tabName}-a1-a2.html`)
+        contentElement.innerHTML = html
+      }
+
+      const hiddenElements = contentElement.querySelectorAll<HTMLElement>(`.hide-abitti-${version}`)
+      hiddenElements.forEach((element) => element.parentNode?.removeChild(element))
+
+      initializeLanguage()
+    }
   }
 }
